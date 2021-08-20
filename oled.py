012fe3be6b2d0ef29ff340bd4b4a76c797ef64bb
niklas.abel <niklas.abel@x41-dsec.de>
2021-08-20 19:18:33 +0200

    change space / tab mix to tabs

--- oled.py
+++ oled.py
@@ -7,19 +7,19 @@ from signal import signal, SIGINT, SIGTERM
 from sys import exit, argv
 
 def signal_handler(sig, frame):
-    try:
-    	# Blank screen on shutdown
-        dev.send_feature_report(bytearray([0x61] + [0x00] * 641))
-        dev.close()
-        print("\n")
-        exit(0)
-    except:
-        exit(0)
+	try:
+		# Blank screen on shutdown
+		dev.send_feature_report(bytearray([0x61] + [0x00] * 641))
+		dev.close()
+		print("\n")
+		exit(0)
+	except:
+		exit(0)
 
 # Check for arguments
 if(len(argv) < 2):
 	print("Usage: oled.py image.gif\n")
-	exit(0)        
+	exit(0)		
 
 # Set up ctrl-c handler
 signal(SIGINT, signal_handler)
@@ -31,10 +31,10 @@ en = Enumeration()
 # Return a list of devices based on the search parameters / Hardcoded to Apex 7
 devices = en.find(vid=0x1038, pid=0x1612, interface=1)
 if not devices:
-    devices = en.find(vid=0x1038, pid=0x1618, interface=1)
+	devices = en.find(vid=0x1038, pid=0x1618, interface=1)
 if not devices:
-    print("No devices found, exiting.")
-    exit(0)
+	print("No devices found, exiting.")
+	exit(0)
 
 # Use first device found with vid/pid
 dev = devices[0]
@@ -46,18 +46,18 @@ im = Image.open(argv[1])
 
 while(1):
 	for frame in ImageSequence.Iterator(im):
-	    
-	    # Image size based on Apex 7
-	    frame = frame.resize((128, 40))
-	    
-	    # Convert to monochrome
-	    frame = frame.convert('1')
-	    data = frame.tobytes()
-	    
-	    # Set up feature report package
-	    data = bytearray([0x61]) + data + bytearray([0x00])
+		
+		# Image size based on Apex 7
+		frame = frame.resize((128, 40))
+		
+		# Convert to monochrome
+		frame = frame.convert('1')
+		data = frame.tobytes()
+		
+		# Set up feature report package
+		data = bytearray([0x61]) + data + bytearray([0x00])
 
-	    dev.send_feature_report(data)
-	    sleep(frame.info['duration'] / 1000)
+		dev.send_feature_report(data)
+		sleep(frame.info['duration'] / 1000)
 
 dev.close()
